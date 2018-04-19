dd5846d20f96a5e09ee03e1f5beee3ec03975ce6
frrist <forrest@protocol.ai>
2018-04-19 12:51:09 -0700

    add event logs to Generate block

--- mining/block_generator.go
+++ mining/block_generator.go
@@ -35,7 +35,14 @@ type blockGenerator struct {
 }
 
 // Generate returns a new block created from the messages in the pool.
-func (b blockGenerator) Generate(ctx context.Context, baseBlock *types.Block, rewardAddress types.Address) (*types.Block, error) {
+func (b blockGenerator) Generate(ctx context.Context, baseBlock *types.Block, rewardAddress types.Address) (blk *types.Block, err error) {
+	ctx = log.Start(ctx, "Generate")
+	defer func() {
+		log.SetTag(ctx, "base-block", baseBlock.Cid().String())
+		log.SetTag(ctx, "reward-address", rewardAddress.String())
+		log.SetTag(ctx, "new-block", blk.Cid().String())
+		log.FinishWithErr(ctx, err)
+	}()
 	stateTree, err := b.getStateTree(ctx, baseBlock.StateRoot)
 	if err != nil {
 		return nil, err
