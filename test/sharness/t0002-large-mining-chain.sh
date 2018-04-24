404e7897de922cd253f3df2347cfaacbee2bcca3
frrist <forrest@protocol.ai>
2018-04-24 13:57:44 -0700

    add 2 example shareness tests calling iptb

---
+++ test/sharness/t0002-large-mining-chain.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+test_description="Test mining command"
+
+. ./sharness.sh
+
+test_expect_success "init iptb nodes" '
+  NUM_NODES=50
+  iptb-private init -n "$NUM_NODES" --type=filecoin --deployment=local
+'
+
+test_expect_success "start iptb cluster" '
+  iptb-private start
+'
+
+test_expect_success "connect iptb nodes" '
+  for ((i=0; i<"$NUM_NODES"; i++))
+  do
+    for ((j=0; j<"$NUM_NODES"; j++))
+    do
+      iptb-private connect "$i" "$j"
+    done
+  done
+'
+
+test_expect_success "each node mines a block" '
+  for ((i=0; i<"$NUM_NODES"; i++))
+  do
+    iptb-private run "$i" go-filecoin mining once
+    sleep 1
+  done
+'
+
+test_expect_success "block chain lenght is correct" '
+  EXPECT=`expr $NUM_NODES + 1`
+  echo "$EXPECT" > expect
+  for ((i=0; i<"$NUM_NODES"; i++))
+  do
+    iptb-private run "$i" go-filecoin chain ls --enc=json | tee debug"$i"  |wc -l > actual
+    test_cmp actual expect
+  done
+'
+
+test_expect_success "stop iptb" '
+  iptb-private stop
+'
+
+test_done
